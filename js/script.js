d67cc7220ab2f5ff5b05ba30f737b2f4d0b77f05
Jay <j4kub07@protonmail.com>
2022-11-24 21:14:59 +0100

    Working verson

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 window.onload = () => {
 	let button = document.querySelector("#btn");
-
 	button.addEventListener("click", calculateBMI);
 };
 
@@ -8,31 +7,31 @@ function calculateBMI() {
 
 	let height = parseInt(document
 			.querySelector("#height").value);
-
 	let weight = parseInt(document
 			.querySelector("#weight").value);
-
 	let result = document.querySelector("#result");
 
 	if (height === "" || isNaN(height))
 		result.innerHTML = "Provide a valid Height!";
-
 	else if (weight === "" || isNaN(weight))
 		result.innerHTML = "Provide a valid Weight!";
-
 	else {
-
 		let bmi = (weight / ((height * height)
 							/ 10000)).toFixed(2);
 
-		if (bmi < 18.6) result.innerHTML =
+		if (bmi < 19) result.innerHTML =
 			`Under Weight : <span>${bmi}</span>`;
-
-		else if (bmi >= 18.6 && bmi < 24.9)
+		else if (bmi >= 19 && bmi < 23.9)
 			result.innerHTML =
-				`Normal : <span>${bmi}</span>`;
-
-		else result.innerHTML =
+			`Normal Weight: <span>${bmi}</span>`;
+		else if (bmi >= 24 && bmi < 28.9) 
+			result.innerHTML =
 			`Over Weight : <span>${bmi}</span>`;
+		else if (bmi >= 29 && bmi < 39) 
+			result.innerHTML =
+			`Obesity : <span>${bmi}</span>`;
+		else 
+			result.innerHTML =
+			`Strong Obesity : <span>${bmi}</span>`;
 	}
 }
